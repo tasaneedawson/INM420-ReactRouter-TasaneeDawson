43e86e2607258add5ec47a780419eb9e9470369d
TDawson22 <jack_hkdgiant@hotmail.com>
2021-07-15 13:49:31 -0400

    working react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              {/* <a href="">something</a> */}
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+            {/* about page */}
+          </Route>
+          <Route path="/users">
+            <Users />
+            {/* users page */}
+          </Route>
+          <Route path="/">
+            <Home />
+            {/* home page */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+   
   );
 }
 
-export default App;
+
+function Home() {
+  return <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Home Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+        Learn React
+      </a>
+    </header>
+  </div>;
+}
+
+function About() {
+  return <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        About Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+        Learn React
+      </a>
+    </header>
+  </div>;
+}
+
+function Users() {
+  return <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Users Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+        Learn React
+      </a>
+    </header>
+  </div>;
+}
+
+
